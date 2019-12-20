27540178c0b1446ea8522505c8be2b0e9be1a155
Robert Silva <robert.silva4@fatec.sp.gov.br>
2019-12-19 22:18:56 -0300

    implementing all verbs development

--- RestWithASPNETUdemy 02 - Using Diferent Verbs/RestWithASPNETUdemy/Controllers/PersonsController.cs
+++ RestWithASPNETUdemy 02 - Using Diferent Verbs/RestWithASPNETUdemy/Controllers/PersonsController.cs
@@ -4,17 +4,27 @@ using RestWithASPNETUdemy.Services;
 
 namespace RestWithASPNETUdemy.Controllers
 {
+    /* Mapeia as requisições de http://localhost:{porta}/api/person/
+    Por padrão o ASP.NET Core mapeia todas as classes que extendem Controller
+    pegando a primeira parte do nome da classe em lower case [Person]Controller
+    e expõe como endpoint REST
+    */
     [Route("api/[controller]")]
     [ApiController]
     public class PersonsController : ControllerBase
     {
+        //declaraçã do servidor usado
         private IPersonService _personService;
 
+        //injeção de uma instância IPersonServices ao criar
+        //uma instancia de persons controllers
         public PersonsController(IPersonService personService)
         {
             _personService = personService;
         }
 
+        //Mapeia as requisições GET para http://localhost:{porta}/api/person/
+        //Get sem parâmetros para o FindAll --> Busca Todos
         // GET api/values
         [HttpGet]
         public IActionResult Get()
@@ -22,6 +32,9 @@ namespace RestWithASPNETUdemy.Controllers
             return Ok(_personService.FindAll());
         }
 
+        //Mapeia as requisições GET para http://localhost:{porta}/api/person/{id}
+        //recebendo um ID como no Path da requisição
+        //Get com parâmetros para o FindById --> Busca Por ID
         // GET api/values/5
         [HttpGet("{id}")]
         public IActionResult Get(int id)
@@ -31,6 +44,8 @@ namespace RestWithASPNETUdemy.Controllers
             return Ok(person);
         }
 
+        //Mapeia as requisições POST para http://localhost:{porta}/api/person/
+        //O [FromBody] consome o Objeto JSON enviado no corpo da requisição
         // POST api/values
         [HttpPost]
         public IActionResult Post([FromBody] Person person)
@@ -39,6 +54,8 @@ namespace RestWithASPNETUdemy.Controllers
             return new ObjectResult(_personService.Create(person));
         }
 
+        //Mapeia as requisições PUT para http://localhost:{porta}/api/person/
+        //O [FromBody] consome o Objeto JSON enviado no corpo da requisição
         // PUT api/values/5
         [HttpPut("{id}")]
         public IActionResult Put([FromBody] Person person)
@@ -47,6 +64,8 @@ namespace RestWithASPNETUdemy.Controllers
             return new ObjectResult(_personService.Update(person));
         }
 
+        //Mapeia as requisições DELETE para http://localhost:{porta}/api/person/{id}
+        //recebendo um ID como no Path da requisição
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
