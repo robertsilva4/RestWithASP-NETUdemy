b0b14a740b3db522c51c5b11fd38f217eb780bd6
ROBERT SILVA <robert.silva4@fatec.sp.gov.br>
2020-03-09 09:02:36 -0300

    Adding Xml Format

---
+++ RestWithASPNETUdemy 10 - Adding Content Negotiation/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -0,0 +1,12 @@
+﻿namespace RestWithASPNETUdemy.Data.VO
+{
+    public class PersonVO
+    {
+        public long? Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
---
+++ RestWithASPNETUdemy 10 - Adding Content Negotiation/RestWithASPNETUdemy/Repository/implementattions/PersonRepositoryImpl.cs
@@ -0,0 +1,91 @@
+﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNETUdemy.Repository.implementattions
+{
+    public class PersonRepositoryImpl : IPersonRepository
+    {
+        private readonly MySQLContext _context;
+        public PersonRepositoryImpl(MySQLContext context)
+        {
+            _context = context;
+        }
+
+
+        //Método responsável por gerar uma nova pessoa
+        //caso houesse um banco de dados seria necessário persistir nos dados
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return person;
+        }
+
+
+        //Método responsável por retornar todas as pessoas 
+        //porém as pessoas no momento são mockus
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        //Método responsáve por retornar uma pessoa
+        //como não acessamos o banco de dados estamos retornando um mock
+        public Person FindById(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        //Método de atualizar as pessoas
+        public Person Update(Person person)
+        {
+            if (!Exist(person.Id)) return null;
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            try
+            {
+                _context.Entry(result).CurrentValues.SetValues(person);
+                _context.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return person;
+        }
+        //Método responsável por deletar uma pessoa através do ID
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            try
+            {
+                if (result != null)
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Exist(long? id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
+        }
+    }
+}
